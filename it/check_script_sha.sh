0d97a2e2801562ba45f2ef78ab9a736f65ebd57d
Ning Shang <syncom.dev@gmail.com>
2022-05-14 15:26:06 -0700

    Generate ScriptSHAs from an embedded file
    Instead of hardcoding known script SHA-256 digest values of `bin/hermit`
    and `bin/activate-hermit`, we now have them read from an embedded file
    named `script.sha256`. This is to make it easier to automate the release
    process. Note that `script.sha256` is supposed to be an append-only
    file.
    
    Add a subcommand `script-sha` to print out ScriptSHAs known to Hermit.
    
    Add a CI integration testing step to make sure file `script.sha` contains
    the sha256 values corresponding to the current source code revision.
    
    Also made an opportunistic change in `it/common.sh` to make
    `fakeRelease` able to work for any channel.

---
+++ it/check_script_sha.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euxo pipefail
+
+SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "${SCRIPT_DIR}/.."
+
+for channel in "canary" "stable"; do
+  rm -rf build
+  # Build on native architecture
+  make CHANNEL="${channel}" build
+  gunzip -c build/hermit*.gz > build/hermit
+  chmod +x build/hermit
+  build/hermit version
+  PROJECT_DIR=$(mktemp -d)
+  build/hermit init "${PROJECT_DIR}"
+  BIN_HERMIT_SHA=$(openssl dgst -sha256 "${PROJECT_DIR}/bin/hermit" | awk '{print $NF}')
+  BIN_ACTIVATE_HERMIT_SHA=$(openssl dgst -sha256 "${PROJECT_DIR}/bin/activate-hermit" | awk '{print $NF}')
+  if ! build/hermit script-sha | grep -q "${BIN_HERMIT_SHA}"; then
+    echo "(${channel}) Script hermit's sha256 ${BIN_HERMIT_SHA} not found. Please add it to file script.sha256."
+    exit 1
+  fi
+  if ! build/hermit script-sha | grep -q "${BIN_ACTIVATE_HERMIT_SHA}"; then
+    echo "(${channel}) Script activate-hermit's sha256 ${BIN_ACTIVATE_HERMIT_SHA} not found. Please add it to file script.sha256."
+    exit 1
+  fi
+done
+echo "File script.sha is up-to-date."
+# Clean up
+rm -rf build
