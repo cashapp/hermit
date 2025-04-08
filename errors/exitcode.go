5bdb8395f11c9f61dd561b8d3c64d38195c71723
Sutina Wipawiwat <swipawiwat@squareup.com>
2025-04-08 15:53:53 +1000

    refactor: stop returning exit code in ui and return custom exit code in main.go

--- errors/exitcode.go
+++ errors/exitcode.go
@@ -1,5 +1,11 @@
 package errors
 
+import (
+	"errors"
+
+	"github.com/alecthomas/kong"
+)
+
 type ExitError struct {
 	err  error
 	code int
@@ -14,4 +20,19 @@ func WithExitCode(err error, code int) error {
 		return nil
 	}
 	return &ExitError{err: err, code: code}
+}
+
+// ExitCodeFromError returns the exit code for the given error.
+// If err implements kong.ExitCoder, ExitCode is used.
+// Otherwise, returns 0 if err == nil, or 1 for a generic failure.
+func ExitCodeFromError(err error) int {
+	var ec kong.ExitCoder
+	switch {
+	case errors.As(err, &ec):
+		return ec.ExitCode()
+	case err == nil:
+		return 0
+	default:
+		return 1
+	}
 }
