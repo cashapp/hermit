cashapp/hermit

commit dd5ca73e8700d016fb6b3ae4269111b38cee0336

feat: add a --[no]-self-update flag + envar (#507)
```
hermit --no-self-update ...
```

or

```
export HERMIT_SELF_UPDATE=false
```

Makefile
@@ -7,6 +7,7 @@ VERSION ?= $(shell git describe --tags --dirty  --always)
 GOOS ?= $(shell ./bin/go version | awk '{print $$NF}' | cut -d/ -f1)
 GOARCH ?= $(shell ./bin/go version | awk '{print $$NF}' | cut -d/ -f2)
 BIN = $(BUILD_DIR)/hermit-$(GOOS)-$(GOARCH)
+export HERMIT_EXE =
 
 .PHONY: all build lint test
 

app/exec_cmd.go
@@ -24,7 +24,16 @@ type execCmd struct {
 	Args   []string `arg:"" help:"Arguments to pass to executable (use -- to separate)." optional:""`
 }
 
-func (e *execCmd) Run(l *ui.UI, cache *cache.Cache, sta *state.State, globalState GlobalState, config Config, defaultHTTPClient *http.Client, sourceRewriters []sources.URLRewriter) error {
+func (e *execCmd) Run(
+	l *ui.UI,
+	cli cliInterface,
+	cache *cache.Cache,
+	sta *state.State,
+	globalState GlobalState,
+	config Config,
+	defaultHTTPClient *http.Client,
+	sourceRewriters []sources.URLRewriter,
+) error {
 	envDir, err := hermit.FindEnvDir(e.Binary)
 	if err != nil {
 		return errors.WithStack(err)
@@ -47,14 +56,9 @@ func (e *execCmd) Run(l *ui.UI, cache *cache.Cache, sta *state.State, globalStat
 	if err != nil {
 		return errors.WithStack(err)
 	}
-
-	// Upgrade hermit if necessary
-	pkgRef := filepath.Base(filepath.Dir(self))
-	if strings.HasPrefix(pkgRef, "hermit@") {
-		err := updateHermit(l, env, pkgRef, false)
-		if err != nil {
-			return errors.WithStack(err)
-		}
+	err = maybeUpdateHermit(l, env, cli.getSelfUpdate(), false)
+	if err != nil {
+		return errors.WithStack(err)
 	}
 
 	// Special-case executing Hermit itself.
@@ -97,7 +101,22 @@ func (e *execCmd) Run(l *ui.UI, cache *cache.Cache, sta *state.State, globalStat
 	return env.Exec(l, pkg, binary, args, deps)
 }
 
-func updateHermit(l *ui.UI, env *hermit.Env, pkgRef string, force bool) error {
+// Upgrade hermit if necessary. We do this to ensure we can read the latest versions of manifests, otherwise they
+// might have configuration that is unavailable in older versions.
+func maybeUpdateHermit(l *ui.UI, env *hermit.Env, selfUpdate bool, force bool) error {
+	if !selfUpdate {
+		return nil
+	}
+	self, err := os.Executable()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	pkgRef := filepath.Base(filepath.Dir(self))
+	if !strings.HasPrefix(pkgRef, "hermit@") {
+		return nil
+	}
+
 	l.Tracef("Checking if %s needs to be updated", pkgRef)
 	pkg, err := env.Resolve(l, manifest.ExactSelector(manifest.ParseReference(pkgRef)), false)
 	if err != nil {

app/update_cmd.go
@@ -1,24 +1,15 @@
 package app
 
 import (
-	"os"
-	"path/filepath"
-	"strings"
-
 	"github.com/cashapp/hermit"
 	"github.com/cashapp/hermit/errors"
-	"github.com/cashapp/hermit/manifest"
 	"github.com/cashapp/hermit/state"
 	"github.com/cashapp/hermit/ui"
 )
 
 type updateCmd struct{}
 
-func (s *updateCmd) Run(l *ui.UI, env *hermit.Env, state *state.State) error {
-	self, err := os.Executable()
-	if err != nil {
-		return errors.WithStack(err)
-	}
+func (s *updateCmd) Run(l *ui.UI, env *hermit.Env, state *state.State, cli cliInterface) error {
 	srcs, err := state.Sources(l)
 	if err != nil {
 		return errors.WithStack(err)
@@ -33,16 +24,9 @@ func (s *updateCmd) Run(l *ui.UI, env *hermit.Env, state *state.State) error {
 		return errors.WithStack(err)
 	}
 	// Upgrade hermit if necessary
-	pkgRef := filepath.Base(filepath.Dir(self))
-	if strings.HasPrefix(pkgRef, "hermit@") {
-		pkg, err := state.Resolve(l, manifest.ExactSelector(manifest.ParseReference(pkgRef)))
-		if err != nil {
-			return errors.WithStack(err)
-		}
-		err = state.UpgradeChannel(l.Task(pkgRef), pkg)
-		if err != nil {
-			return errors.WithStack(err)
-		}
+	err = maybeUpdateHermit(l, env, cli.getSelfUpdate(), true)
+	if err != nil {
+		return errors.WithStack(err)
 	}
 	return nil
 }

app/commands.go
@@ -20,6 +20,7 @@ type cliInterface interface {
 	getDebug() bool
 	getTrace() bool
 	getQuiet() bool
+	getSelfUpdate() bool
 	getLevel() ui.Level
 	getGlobalState() GlobalState
 	getLockTimeout() time.Duration
@@ -33,6 +34,7 @@ type cliBase struct {
 	Debug          bool             `help:"Enable debug logging." short:"d"`
 	Trace          bool             `help:"Enable trace logging." short:"t"`
 	Quiet          bool             `help:"Disable logging and progress UI, except fatal errors." env:"HERMIT_QUIET" short:"q"`
+	SelfUpdate     bool             `help:"If true (the default) Hermit will update itself automatically." negatable:"" default:"true" env:"HERMIT_SELF_UPDATE"`
 	Level          ui.Level         `help:"Set minimum log level (${enum})." env:"HERMIT_LOG" default:"auto" enum:"auto,trace,debug,info,warn,error,fatal"`
 	LockTimeout    time.Duration    `help:"Timeout for waiting on the lock" default:"30s" env:"HERMIT_LOCK_TIMEOUT"`
 	UserConfigFile string           `help:"Path to Hermit user configuration file." name:"user-config" default:"~/.hermit.hcl" env:"HERMIT_USER_CONFIG"`
@@ -62,6 +64,7 @@ func (u *cliBase) getMemProfile() string         { return u.MemProfile }
 func (u *cliBase) getTrace() bool                { return u.Trace }
 func (u *cliBase) getDebug() bool                { return u.Debug }
 func (u *cliBase) getQuiet() bool                { return u.Quiet }
+func (u *cliBase) getSelfUpdate() bool           { return u.SelfUpdate }
 func (u *cliBase) getLevel() ui.Level            { return ui.AutoLevel(u.Level) }
 func (u *cliBase) getGlobalState() GlobalState   { return u.GlobalState }
 func (u *cliBase) getLockTimeout() time.Duration { return u.LockTimeout }

commit 29578307b931cf43572eb25ea40823d602ff075e

fix: don't rely on an environment when self updating
This was a regression I introduced in #507.

.github/workflows/release.yml
@@ -1,9 +1,9 @@
 on:
   push:
     paths-ignore:
-      - 'docs/**'
+      - "docs/**"
     tags:
-      - 'v*'
+      - "v*"
 name: Release
 jobs:
   deployable:
@@ -14,7 +14,7 @@ jobs:
         uses: actions/checkout@08c6903cd8c0fde910a37f88322edcfb5dd907a8 # v5
       - uses: actions/setup-go@d35c59abb061a4a6fb18e82ac0862c26744d6ab5 # v5
         with:
-          go-version: '^1.16.3'
+          go-version: "^1.16.3"
       - name: Build Hermit
         run: |
           make GOOS=linux GOARCH=amd64 CHANNEL=stable build

Makefile
@@ -23,7 +23,7 @@ test-integration: ## run integration tests
 	./bin/go test -count=1 -tags integration -v ./integration
 
 build: build-binary ## builds binary and gzips it
-	gzip -9 $(BIN)
+	gzip -f9 $(BIN)
 
 build-binary: ## builds binary
 	mkdir -p $(BUILD_DIR)

app/exec_cmd.go
@@ -56,7 +56,7 @@ func (e *execCmd) Run(
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	err = maybeUpdateHermit(l, env, cli.getSelfUpdate(), false)
+	err = maybeUpdateHermit(l, sta, cli.getSelfUpdate(), false)
 	if err != nil {
 		return errors.WithStack(err)
 	}
@@ -103,7 +103,9 @@ func (e *execCmd) Run(
 
 // Upgrade hermit if necessary. We do this to ensure we can read the latest versions of manifests, otherwise they
 // might have configuration that is unavailable in older versions.
-func maybeUpdateHermit(l *ui.UI, env *hermit.Env, selfUpdate bool, force bool) error {
+//
+// If "selfUpdate" is false this will be a no-op under all circumstances.
+func maybeUpdateHermit(l *ui.UI, state *state.State, selfUpdate bool, force bool) error {
 	if !selfUpdate {
 		return nil
 	}
@@ -118,7 +120,7 @@ func maybeUpdateHermit(l *ui.UI, env *hermit.Env, selfUpdate bool, force bool) e
 	}
 
 	l.Tracef("Checking if %s needs to be updated", pkgRef)
-	pkg, err := env.Resolve(l, manifest.ExactSelector(manifest.ParseReference(pkgRef)), false)
+	pkg, err := state.Resolve(l, manifest.ExactSelector(manifest.ParseReference(pkgRef)))
 	if err != nil {
 		return errors.WithStack(err)
 	}
@@ -128,7 +130,7 @@ func maybeUpdateHermit(l *ui.UI, env *hermit.Env, selfUpdate bool, force bool) e
 	} else if pkg.UpdatedAt.IsZero() {
 		pkg.UpdatedAt = time.Now()
 	}
-	err = env.UpdateUsage(pkg)
+	err = state.WritePackageState(pkg)
 	if err != nil {
 		return errors.WithStack(err)
 	}
@@ -137,5 +139,5 @@ func maybeUpdateHermit(l *ui.UI, env *hermit.Env, selfUpdate bool, force bool) e
 		// set the update time to 0 to force an update check
 		pkg.UpdatedAt = time.Time{}
 	}
-	return errors.WithStack(env.EnsureChannelIsUpToDate(l, pkg))
+	return errors.WithStack(state.EnsureChannelIsUpToDate(l, pkg))
 }

app/update_cmd.go
@@ -24,7 +24,7 @@ func (s *updateCmd) Run(l *ui.UI, env *hermit.Env, state *state.State, cli cliIn
 		return errors.WithStack(err)
 	}
 	// Upgrade hermit if necessary
-	err = maybeUpdateHermit(l, env, cli.getSelfUpdate(), true)
+	err = maybeUpdateHermit(l, state, cli.getSelfUpdate(), true)
 	if err != nil {
 		return errors.WithStack(err)
 	}

env.go
@@ -1170,13 +1170,7 @@ func (e *Env) Search(l *ui.UI, pattern string) (manifest.Packages, error) {
 //
 // This should only be called for packages that have already been installed
 func (e *Env) EnsureChannelIsUpToDate(l *ui.UI, pkg *manifest.Package) error {
-	task := l.Task(pkg.Reference.String())
-	if pkg.UpdateInterval == 0 || pkg.UpdatedAt.After(time.Now().Add(-1*pkg.UpdateInterval)) {
-		task.Tracef("No updated required")
-		// No updates needed for this package
-		return nil
-	}
-	return errors.WithStack(e.state.UpgradeChannel(task, pkg))
+	return errors.WithStack(e.state.EnsureChannelIsUpToDate(l, pkg))
 }
 
 // AddSource adds a new source bundle and refreshes the packages from it

state/state.go
@@ -505,6 +505,21 @@ func (s *State) CleanCache(b ui.Logger) error {
 	return os.RemoveAll(s.cacheDir)
 }
 
+// EnsureChannelIsUpToDate updates the package if it has an update interval,
+// the required time since the last update check has passed,
+// and the etag in the source has changed from the last check.
+//
+// This should only be called for packages that have already been installed
+func (s *State) EnsureChannelIsUpToDate(l *ui.UI, pkg *manifest.Package) error {
+	task := l.Task(pkg.Reference.String())
+	if pkg.UpdateInterval == 0 || pkg.UpdatedAt.After(time.Now().Add(-1*pkg.UpdateInterval)) {
+		task.Tracef("No updated required")
+		// No updates needed for this package
+		return nil
+	}
+	return errors.WithStack(s.UpgradeChannel(task, pkg))
+}
+
 // UpgradeChannel checks if the given binary has changed in its channel, and if so, downloads it.
 //
 // If the channel is upgraded this will return a clone of the updated manifest.